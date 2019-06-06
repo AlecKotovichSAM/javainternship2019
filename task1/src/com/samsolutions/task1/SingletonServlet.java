ea94a73e11ca812556835584a7ecb0ae2d1a06b7
Aleg Katovich <a.katovich@sam-solutions.com>
2019-06-06 16:50:09 +0300

    Task1 - SingletonServlet

---
+++ task1/src/com/samsolutions/task1/SingletonServlet.java
@@ -0,0 +1,54 @@
+package com.samsolutions.task1;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SingletonServlet
+ */
+@WebServlet("/singletonServlet")
+public class SingletonServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+
+	
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ")
+		.append(request.getContextPath())
+		.append(" by ")
+		.append(this.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+	
+
+
+}
